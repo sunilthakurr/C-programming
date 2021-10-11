1ddba83ce779c5d3ca605010a4b8928af76a695c
SUNIL KUMAR <91265823+sunilthakurr@users.noreply.github.com>
2021-10-11 11:27:07 +0530

    Addition of two integers

---
+++ add two inegers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter the first integer");
+    scanf("%d",&a);
+    printf("Enter the second integer");
+    scanf("%d",&b);
+    printf("The sum of %d and %d is %d",a,b,a+b);
+    return 0;
+}
