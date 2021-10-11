25c54ae192ca9ed751a82a2131d0b33044826faa
SUNIL KUMAR <91265823+sunilthakurr@users.noreply.github.com>
2021-10-11 11:28:40 +0530

    Product of two integers

---
+++ product of two floating point numbers .c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float a,b;
+    printf("Enter the first number");
+    scanf("%f",&a);
+    printf("Enter the second number");
+    scanf("%f",&b);
+    printf("The product of %f and %f is %f",a,b,a*b);
+    return 0;
+}
