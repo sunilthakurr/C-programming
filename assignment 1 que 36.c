21061306b59fe9f3ca4df554a26d7976d30a71a2
SUNIL KUMAR <91265823+sunilthakurr@users.noreply.github.com>
2021-10-16 18:56:40 +0530

    Add files via upload

---
+++ assignment 1 que 36.c
@@ -0,0 +1,13 @@
+/* Raju got 6 and half apples from each of Raghu, Sheemu and Akash .
+He wants to know how many apples he has in total without adding them.
+Write a program which could help Raju in doing this . */
+
+
+
+#include<stdio.h>
+int main()
+{
+    float a=6.5;
+    printf("No. of apples Raju have is %f",a*3);
+    return 0;
+}
