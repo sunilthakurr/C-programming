26eedbe8b210a1a210a33780950bfd65ba76d37b
SUNIL KUMAR <91265823+sunilthakurr@users.noreply.github.com>
2021-12-26 09:22:27 +0530

    Add files via upload

---
+++ 14.Arrays assignment.c
@@ -0,0 +1,33 @@
+/*WAP to find an array of N number of  elements(elements can repeat). Input an element you want to search and find it. 
+If found then print all the position of that element otherwise print not found. */
+
+
+#include<stdio.h>
+int
+main ()
+{
+  int A, n, flag = 0, x;
+  printf ("Enter the size of array\n");
+  scanf ("%d", &n);
+  printf ("Enter the elements of array\n");
+  int a[n], i;
+  for (i = 0; i < n; i++)
+    {
+      scanf ("%d", &a[i]);
+    }
+  printf ("Enter the elements you want to search\n");
+  scanf ("%d", &A);
+  for (i = 0; i < n; i++)
+    {
+      flag++;
+      if (a[i] == A)
+	{
+	  x = 1;
+	  printf ("The elements you want to search is at %d position\n",
+		  flag);
+	}
+    }
+  if (x != 1)
+    printf ("Not found");
+  return 0;
+}
